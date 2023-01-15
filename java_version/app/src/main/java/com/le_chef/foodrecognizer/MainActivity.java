86f9d434ba99904827f71a56ced637db373e6774
Raul Zarate <101022036+zaratr@users.noreply.github.com>
2023-01-15 11:16:35 -0800

    init java project

---
+++ java_version/app/src/main/java/com/le_chef/foodrecognizer/MainActivity.java
@@ -0,0 +1,14 @@
+package com.le_chef.foodrecognizer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
