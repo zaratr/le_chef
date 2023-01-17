911b4b4aee819a30a2e2afd6f6d5593d34abcbd3
Raul Zarate <101022036+zaratr@users.noreply.github.com>
2023-01-16 22:55:33 -0800

    added python code

--- python_model_maker/main.py
+++ python_model_maker/main.py
@@ -1,14 +1,81 @@
-# This is a sample Python script.
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+img_height, img_width = 32, 32
+batch_size = 20
+train_ds = tf.keras.utils.image_dataset_from_directory(
+    "fruits/train",
+    image_size = (img_height, img_width),
+    batch_size = batch_size
+)
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+    "fruits/validation",
+    image_size = (img_height, img_width),
+    batch_size = batch_size
+)
+
+test_ds = tf.keras.utils.image_dataset_from_directory(
+    "fruits/test",
+    image_size = (img_height, img_width),
+    batch_size = batch_size
+)
+
+class_names = ["apple", "banana", "orange"]
+plt.figure(figsize=(10,10))
+for images, labels in train_ds.take(1):
+  for i in range(9):
+    ax = plt.subplot(3, 3, i +1)
+    plt.imshow(images[i].numpy().astype("uint8"))
+    plt.title(class_names[labels[i]])
+    plt.axis("off")
+
+model = tf.keras.Sequential(
+    [
+        tf.keras.layers.Rescaling(1. / 255),
+        tf.keras.layers.Conv2D(32, 3, activation="relu"),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.Conv2D(32, 3, activation="relu"),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.Conv2D(32, 3, activation="relu"),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(128, activation="relu"),
+        tf.keras.layers.Dense(3)
+    ]
+
+)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+model.compile(
+    optimizer="adam",
+    loss=tf.losses.SparseCategoricalCrossentropy(from_logits = True),
+    metrics=['accuracy']
+)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+model.fit(
+    train_ds,
+    validation_data = val_ds,
+    epochs = 10
+)
+
+
+model.evaluate(test_ds)
+
+
+plt.figure(figsize=(10,10))
+for images, labels in test_ds.take(1):
+  classifications = model(images)
+  #print(classifications)
+  for i in range(9):
+    ax = plt.subplot(3,3,i+1)
+    plt.imshow(images[i].numpy().astype("uint8"))
+    index = numpy.argmax(classifications[i])
+    plt.title("Pred: " + class_names[index] +  " | Real: " + class_names[labels[i]])
+
+converter= tf.lite.TFLiteConverter.from_keras_model(model)
+tflite_model = converter.convert()
+
+with open("model.tflite", 'wb') as f:
+  f.write(tflite_model)
