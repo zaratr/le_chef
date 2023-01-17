e807d7e0cb6eacab38f872130c1dad0e74c1b8cb
Raul Zarate <101022036+zaratr@users.noreply.github.com>
2023-01-16 20:34:08 -0800

    added init for java version

--- java_version/app/src/main/java/com/le_chef/foodrecognizer/MainActivity.java
+++ java_version/app/src/main/java/com/le_chef/foodrecognizer/MainActivity.java
@@ -18,36 +18,39 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-import com.le_chef.foodrecognizer.ml.Fruits1;
+import com.le_chef.foodrecognizer.ml.OldFruits;
 
 import org.tensorflow.lite.DataType;
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button camera;
+    TextView result, confidence;
     ImageView imageView;
-    TextView tvPlaceHolder;
-    int image_size = 32; //android camera uses 32 so resize everything to 32
+    Button picture;
+    int imageSize = 32;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        camera = findViewById(R.id.captureImageFab);
+        result = findViewById(R.id.result);
+        confidence = findViewById(R.id.confidence);
         imageView = findViewById(R.id.imageView);
-        tvPlaceHolder = findViewById(R.id.tvPlaceholder);
+        picture = findViewById(R.id.button);
 
-        camera.setOnClickListener(new View.OnClickListener() {
+        picture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
                     Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    startActivityForResult(cameraIntent, 3);
+                    startActivityForResult(cameraIntent, 1);
                 }
                 else{
                     requestPermissions(new String[]{Manifest.permission.CAMERA}, 100);
@@ -69,62 +72,49 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         if (resultCode == RESULT_OK) {
-            if (resultCode == 3) {//camera capture access
+            if (requestCode == 1) {//camera capture access
                 Bitmap image = (Bitmap) data.getExtras().get("data");
-                int dimensions = Math.min(image.getWidth(), image.getHeight());
-                image = ThumbnailUtils.extractThumbnail(image, dimensions, dimensions);
+                int dimension = Math.min(image.getWidth(), image.getHeight());
+                image = ThumbnailUtils.extractThumbnail(image, dimension, dimension);
                 imageView.setImageBitmap(image);
 
-                image = Bitmap.createScaledBitmap(image, image_size, image_size, false);
+                image = Bitmap.createScaledBitmap(image, imageSize, imageSize, false);
                 classifyImage(image);
-            }
-            else if(resultCode == 1){//gallery access
-                Uri dat = data.getData();
-                Bitmap image = null;
-                try{
-                    image = MediaStore.Images.Media.getBitmap(this.getContentResolver(), dat);
-                }
-                catch(IOException e){
-                    e.printStackTrace();
-                }
-                imageView.setImageBitmap(image);
-
-                image = Bitmap.createScaledBitmap(image, image_size, image_size, false);
-                classifyImage(image);
-            }
-            else{//image_one, two and three is selected instead.
-
             }
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
 
+
     public void classifyImage(Bitmap image)
     {
         try {
-            Fruits1 model = Fruits1.newInstance(getApplicationContext());
+            OldFruits model = OldFruits.newInstance(getApplicationContext());
 
             // Creates inputs for reference.
             TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 32, 32, 3}, DataType.FLOAT32);
-            ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4* image_size * image_size * 3);//4 -floats * float_pixels^squared * 3 rgb on each image
-            inputFeature0.loadBuffer(byteBuffer);
+            ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4* imageSize * imageSize * 3);//4 -floats * float_pixels^squared * 3 rgb on each image
+            byteBuffer.order(ByteOrder.nativeOrder());
 
-            int[] intValues = new int[image_size*image_size];
+            //get 1D array of 224 * 224 pixels in image
+            int[] intValues = new int[imageSize*imageSize];
             image.getPixels(intValues, 0, image.getWidth(), 0, 0, image.getWidth(), image.getHeight());
+
+            //iterate over pixels and extract R, G and B values. Add to byteBuffer
             int pixel = 0 ;
-            for(int i = 0; i < image_size; ++i){
-                for(int j = 0; i < image_size; ++j){
-                    int val = intValues[++pixel];//RGB
-                    byteBuffer.putFloat(((val >> 16) & 0xFF) * (1.f / 1));//change to 1.f/255 if model didn't rescale in python
-                    byteBuffer.putFloat(((val >> 8) & 0xFF) * (1.f / 1));//change to 1.f/255 if model didn't rescale in python
-                    byteBuffer.putFloat(((val >> 0xFF) & 0xFF) * (1.f / 1));//change to 1.f/255 if model didn't rescale in python
+            for(int i = 0; i < imageSize; i++){
+                for(int j = 0; j < imageSize; j++){
+                    int val = intValues[pixel++];//RGB
+                    byteBuffer.putFloat(((val >> 16) & 0xFF) * (1.f / 1));
+                    byteBuffer.putFloat(((val >> 8) & 0xFF) * (1.f / 1));
+                    byteBuffer.putFloat(((val >> 0xFF) & 0xFF) * (1.f / 1));
                 }
             }
 
             inputFeature0.loadBuffer(byteBuffer);
 
             // Runs model inference and gets result.
-            Fruits1.Outputs outputs = model.process(inputFeature0);
+            OldFruits.Outputs outputs = model.process(inputFeature0);
             TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
 
             //get position of highest confidence
@@ -140,7 +130,7 @@ public class MainActivity extends AppCompatActivity {
             //classes that I used in model creation
             String[] classes = {"Apple", "Banana", "Orange"};
             //display results
-            tvPlaceHolder.setText(classes[maxPos]);
+            result.setText(classes[maxPos]);
 
             // Releases model resources if no longer used.
             model.close();
@@ -151,23 +141,3 @@ public class MainActivity extends AppCompatActivity {
     }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
